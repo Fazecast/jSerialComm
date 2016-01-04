f55a1af7e73cf14f4976ab720c210500d8e255be
hedgecrw85 <will.hedgecock@fazecast.com>
2016-01-04 10:43:42 -0600

    Updated to version 1.3.11.

--- src/test/java/com/fazecast/jSerialComm/SerialPortTest.java
+++ src/test/java/com/fazecast/jSerialComm/SerialPortTest.java
@@ -32,7 +32,7 @@ import java.util.Scanner;
  * This class provides a test case for the jSerialComm library.
  * 
  * @author Will Hedgecock &lt;will.hedgecock@gmail.com&gt;
- * @version 1.3.10
+ * @version 1.3.11
  * @see java.io.InputStream
  * @see java.io.OutputStream
  */
