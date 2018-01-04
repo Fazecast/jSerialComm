45b426a68f022dd03decb6e6136c07efcc89c9d9
hedgecrw85 <will.hedgecock@fazecast.com>
2018-01-03 18:27:13 -0600

    Updated licensing to dual-license LGPLv3 and Apache License v2

--- src/main/c/Android/jni/AndroidHelperFunctions.h
+++ src/main/c/Android/jni/AndroidHelperFunctions.h
@@ -5,22 +5,22 @@
  *  Last Updated on:  Mar 25, 2016
  *           Author:  Will Hedgecock
  *
- * Copyright (C) 2012-2017 Fazecast, Inc.
+ * Copyright (C) 2012-2018 Fazecast, Inc.
  *
  * This file is part of jSerialComm.
  *
  * jSerialComm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of either the Apache Software License, version 2, or
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, version 3 or above.
  *
  * jSerialComm is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with jSerialComm.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of both the GNU Lesser General Public
+ * License and the Apache Software License along with jSerialComm. If not,
+ * see <http://www.gnu.org/licenses/> and <http://www.apache.org/licenses/>.
  */
 
 #ifndef __ANDROID_HELPER_FUNCTIONS_HEADER_H__
--- src/main/c/OSX/SerialPort_OSX.c
+++ src/main/c/OSX/SerialPort_OSX.c
@@ -10,17 +10,17 @@
  * This file is part of jSerialComm.
  *
  * jSerialComm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of either the Apache Software License, version 2, or
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, version 3 or above.
  *
  * jSerialComm is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with jSerialComm.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of both the GNU Lesser General Public
+ * License and the Apache Software License along with jSerialComm. If not,
+ * see <http://www.gnu.org/licenses/> and <http://www.apache.org/licenses/>.
  */
 
 #ifdef __APPLE__
