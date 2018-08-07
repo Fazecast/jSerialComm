93e125104fe958478c7ea8fc7bc36e3f5d8fb2e9
hedgecrw85 <will.hedgecock@fazecast.com>
2018-08-07 09:41:30 -0500

    Update source version number to 2.1.2

--- src/main/java/com/fazecast/jSerialComm/SerialPort.java
+++ src/main/java/com/fazecast/jSerialComm/SerialPort.java
@@ -42,7 +42,7 @@ import java.util.Date;
  * This class provides native access to serial ports and devices without requiring external libraries or tools.
  *
  * @author Will Hedgecock &lt;will.hedgecock@fazecast.com&gt;
- * @version 2.1.1
+ * @version 2.1.2
  * @see java.io.InputStream
  * @see java.io.OutputStream
  */
