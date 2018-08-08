399105718743937d69861122aec0bc9524e86c52
hedgecrw85 <will.hedgecock@fazecast.com>
2018-08-08 12:44:01 -0500

    Update javadoc link URL and version number

---
+++ src/main/java/com/fazecast/jSerialComm/SerialPortTimeoutException.java
@@ -0,0 +1,83 @@
+/*
+ * SerialPortTimeoutException.java
+ *
+ *       Created on:  Aug 08, 2018
+ *  Last Updated on:  Aug 08, 2018
+ *           Author:  Will Hedgecock
+ *
+ * Copyright (C) 2018-2018 Fazecast, Inc.
+ *
+ * This file is part of jSerialComm.
+ *
+ * jSerialComm is free software: you can redistribute it and/or modify
+ * it under the terms of either the Apache Software License, version 2, or
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, version 3 or above.
+ *
+ * jSerialComm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of both the GNU Lesser General Public
+ * License and the Apache Software License along with jSerialComm. If not,
+ * see <http://www.gnu.org/licenses/> and <http://www.apache.org/licenses/>.
+ */
+
+package com.fazecast.jSerialComm;
+
+import java.io.IOException;
+
+/**
+ * This class describes a serial port timeout exception.
+ * 
+ * @author Will Hedgecock &lt;will.hedgecock@fazecast.com&gt;
+ * @version 2.2.0
+ * @see java.util.EventObject
+ */
+public final class SerialPortTimeoutException extends IOException
+{
+	private static final long serialVersionUID = 3209035213903386044L;
+
+	/**
+	 * Constructs a {@link SerialPortTimeoutException} with {@code null} as its error detail message.
+	 */
+	public SerialPortTimeoutException()
+	{
+		super();
+	}
+
+	/**
+	 * Constructs a {@link SerialPortTimeoutException} with the specified detail message.
+	 * 
+	 * @param message The detail message (which is saved for later retrieval by the {@link getMessage()} method).
+	 */
+	public SerialPortTimeoutException(String message)
+	{
+		super(message);
+	}
+
+	/**
+	 * Constructs a {@link SerialPortTimeoutException} with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with {@link cause} is <i>not</i> automatically incorporated into this exception's detail message.
+	 * 
+	 * @param message message The detail message (which is saved for later retrieval by the {@link getMessage()} method).
+	 * @param cause The cause (which is saved for later retrieval by the {@link getCause()} method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+	 */
+	public SerialPortTimeoutException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a {@link SerialPortTimeoutException} with the specified cause and a detail message of {@code (cause==null ? null : cause.toString()) }
+	 * (which typically contains the class and detail message of {@code cause}). This constructor is useful for IO exceptions that are little more
+	 * than wrappers for other throwables.
+	 * 
+	 * @param cause The cause (which is saved for later retrieval by the {@link getCause()} method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+	 */
+	public SerialPortTimeoutException(Throwable cause)
+	{
+		super(cause);
+	}
+}
