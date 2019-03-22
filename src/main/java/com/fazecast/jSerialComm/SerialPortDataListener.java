206d1721a934e57b21cba6ada552e6d2c1e0ce3f
hedgecrw85 <will.hedgecock@fazecast.com>
2019-03-22 10:43:24 -0500

    Update build version to 2.5.1

--- src/main/java/com/fazecast/jSerialComm/SerialPortDataListener.java
+++ src/main/java/com/fazecast/jSerialComm/SerialPortDataListener.java
@@ -31,7 +31,7 @@ import java.util.EventListener;
  * This interface must be implemented to enable simple event-based serial port I/O.
  * 
  * @author Will Hedgecock &lt;will.hedgecock@fazecast.com&gt;
- * @version 2.5.0
+ * @version 2.5.1
  * @see java.util.EventListener
  */
 public interface SerialPortDataListener extends EventListener
