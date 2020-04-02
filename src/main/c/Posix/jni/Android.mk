05ecc50de874f365848c48b9e5e3eed072c9cd31
hedgecrw85 <will.hedgecock@fazecast.com>
2020-04-02 00:00:11 -0500

    Android build different based on OS

--- src/main/c/Posix/jni/Android.mk
+++ src/main/c/Posix/jni/Android.mk
@@ -10,5 +10,11 @@ LOCAL_CFLAGS := -fsigned-char
 
 include $(BUILD_SHARED_LIBRARY)
 
+HOST_OS := $(strip $(HOST_OS))
+ifeq ($(OS),Windows_NT)
 all:
 	rmdir /Q /S libs obj
+else
+all:
+	rm -rf libs obj
+endif
