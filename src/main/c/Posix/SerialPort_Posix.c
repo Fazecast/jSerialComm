d255a25b24f995d0a66328ac9856fe891be89b8f
Will Hedgecock <will.hedgecock@gmail.com>
2024-04-17 12:39:00 -0500

    Version bump

--- src/main/c/Posix/SerialPort_Posix.c
+++ src/main/c/Posix/SerialPort_Posix.c
@@ -85,7 +85,7 @@ jfieldID eventFlagsField;
 char portsEnumerated = 0;
 char classInitialized = 0;
 pthread_mutex_t criticalSection;
-const char nativeLibraryVersion[] = "2.11.0";
+const char nativeLibraryVersion[] = "2.11.1";
 serialPortVector serialPorts = { NULL, 0, 0 };
 
 // JNI exception handler
